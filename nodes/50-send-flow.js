34edf6404d0debdb4a9c0e4492006c83465fbbf3
Gerrit Riessen <gorenje@posteo.de>
2023-08-24 20:52:20 +0200

    added SendFlow node

---
+++ nodes/50-send-flow.js
@@ -0,0 +1,112 @@
+module.exports = function(RED) {
+  function SendFlowFunctionality(config) {
+    RED.nodes.createNode(this,config);
+
+    var node = this;
+    var cfg = config;
+
+    node.on('close', function() {
+      node.status({});
+    });
+
+    node.on("input", function(msg, send, done) {
+      var sendFlow = (hdrs, got) => {
+        got.post( cfg.hostUrl + "/flows", {
+          headers: {
+            "Node-RED-API-Version":     cfg.flowVersion,
+            "Content-type":	            "application/json",
+            "Node-RED-Deployment-Type": "full",
+            ...hdrs
+          },
+          body: JSON.stringify(msg.payload)
+        }).then( res => {
+          var bodySize = res.body.length;
+
+          send({
+            ...msg,
+            payload: res.body
+          });
+
+          node.status({fill:"green",shape:"dot",text:"Good"});
+          setTimeout( function() {
+            node.status({})
+          }, 450);
+
+        }).catch( err => {
+          node.status({fill:"red",shape:"dot",text:"Failed"});
+          node.error(err)
+        });
+      };
+
+      /**
+      * Authentication
+      **/
+      if ( cfg.useAuthentication ) {
+        var username = undefined;
+        var password = undefined;
+
+        node.status({fill:"blue",shape:"dot",text:"Requesting token"});
+
+        RED.util.evaluateNodeProperty(cfg.apiUsername, cfg.apiUsernameType,
+                                      node, msg, (err, result) => {
+          if (err) {
+            node.status({fill:"red",shape:"dot",text:"Failed"});
+            node.error(err)
+          } else {
+            username = result;
+
+            RED.util.evaluateNodeProperty(cfg.apiPassword, cfg.apiPasswordType,
+                                          node, msg, (err, result) => {
+              if (err) {
+                node.status({fill:"red",shape:"dot",text:"Failed"});
+                node.error(err)
+              } else {
+                password = result;
+
+                var data = {
+                      "client_id":  "node-red-admin",
+                      "grant_type": "password",
+                      "scope":      "*",
+                      "username":   username,
+                      "password":   password
+                }
+
+                import('got').then( (module) => {
+                  module.got.post( cfg.hostUrl + "/auth/token", {
+                    json: data
+                  }).then( res => {
+                    node.status({
+                      fill:"blue",
+                      shape:"dot",
+                      text:"Sending flow"
+                    });
+
+                    var access_token = JSON.parse(res.body).access_token;
+
+                    sendFlow({
+                      "Authorization": "Bearer " + access_token
+                    }, module.got);
+
+                  }).catch((err) => {
+                    node.status({fill:"red",shape:"dot",text:"Failed"});
+                    node.error( err );
+                  });
+                });
+              }
+            })
+          }
+        })
+      } else {
+        /*
+         * Authentication free zone...
+         */
+        node.status({fill:"blue",shape:"dot",text:"Sending flow"});
+        import('got').then( (module) => {
+          sendFlow({}, module.got);
+        });
+      }
+    });
+  }
+
+  RED.nodes.registerType("SendFlow", SendFlowFunctionality);
+}
