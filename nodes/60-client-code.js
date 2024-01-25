af061e998bd5b2ba29bf7ffcfa61ea8cf2498b58
Gerrit Riessen <gerrit@openmindmap.org>
2024-01-25 19:45:03 +0100

    added uglify-js for js code nodes

--- nodes/60-client-code.js
+++ nodes/60-client-code.js
@@ -1,4 +1,6 @@
 module.exports = function(RED) {
+  let UglifyJS = require('uglify-js')
+
   function ClientCodeFunctionality(config) {
     RED.nodes.createNode(this,config);
 
@@ -26,27 +28,26 @@ module.exports = function(RED) {
   RED.nodes.registerType("ClientCode", ClientCodeFunctionality);
 
   RED.httpAdmin.post("/ClientCode/:id",
-                     RED.auth.needsPermission("ClientCode.write"),
-                     (req,res) => {
-                       var node = RED.nodes.getNode(req.params.id);
-                       if (node != null) {
-                         try {
-                           if (req.body && node.type == "ClientCode" ) {
-                             node.send(req.body);
-                             res.sendStatus(200);
-                           } else {
-                             res.sendStatus(404);
-                           }
-                         } catch(err) {
-                           res.sendStatus(500);
-                           node.error("ClientCode: Submission failed: " +
-                                      err.toString())
-                         }
-                       } else {
-                         res.sendStatus(404);
-                       }
-                     });
-
+    RED.auth.needsPermission("ClientCode.write"),
+    (req,res) => {
+      var node = RED.nodes.getNode(req.params.id);
+      if (node != null) {
+        try {
+          if (req.body && node.type == "ClientCode" ) {
+            node.send(req.body);
+            res.sendStatus(200);
+          } else {
+            res.sendStatus(404);
+          }
+        } catch(err) {
+          res.sendStatus(500);
+          node.error("ClientCode: Submission failed: " +
+                    err.toString())
+        }
+      } else {
+        res.sendStatus(404);
+      }
+    });
 
   RED.httpAdmin.post("/ClientCode/:id/status",
     RED.auth.needsPermission("ClientCode.write"),
@@ -69,5 +70,44 @@ module.exports = function(RED) {
         res.sendStatus(404);
       }
     });
+
+  RED.httpAdmin.post("/ClientCode/:id/ugify",
+    RED.auth.needsPermission("ClientCode.write"),
+    (req, res) => {
+      var node = RED.nodes.getNode(req.params.id);
+      if (node != null) {
+        try {
+          if (req.body ) {
+            req.body.nodes.forEach( n => {
+
+              if ( n.format == "javascript") { 
+                /* this handles PkgFile nodes and template nodes */
+                let result = UglifyJS.minify(n.template, req.body.cfg)
+                if ( result.code && !result.error) {               
+                  n.template = result.code
+                }
+              }
+
+              if (n.type == "function") {
+                let result = UglifyJS.minify(n.func, req.body.cfg)
+                if (result.code && !result.error) {
+                  n.func = result.code
+                }
+              }
+            })
+            res.status(200).send(req.body.nodes);
+          } else {
+            res.sendStatus(404);
+          }
+        } catch (err) {
+          res.sendStatus(500);
+          node.error("ClientCode: Submission failed: " +
+            err.toString())
+        }
+      } else {
+        res.sendStatus(404);
+      }
+    });
+
 
 }
